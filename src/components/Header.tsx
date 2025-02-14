3e89027d918bec170ccf78220f1e2cea6f20fa5b
Enmanuel <end199615@gmail.com>
2025-02-14 10:25:36 -0500

    provisional header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,29 @@
 function Header() {
-    // Cambiar class por className
-    return <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    // Cambiar className por className
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="src/assets/arteIdeas.jpg" alt="ArteIdeas" width={134} height={51} />
+                </a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Servicios</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+                <button className="btn btn-dark" type="submit">Marcos Galería</button>
+            </div>
+        </nav>
+    )
 }
 
 // Siempre va esta exportación
-export default Header();
+export default Header;
---
+++ src/components/Screens/Formulario/Iconos.tsx
@@ -0,0 +1,11 @@
+function Iconos() {
+  return (
+    <div>
+        <img src="src\assets\image.svg" alt="icon" width={24} height={24}/>
+        <img src="src\assets\marco.png" alt="icon" width={24} height={24}/>
+        <img src="src\assets\passpartu.png" alt="icon" width={24} height={24}/>
+    </div>
+  )
+}
+
+export default Iconos;
