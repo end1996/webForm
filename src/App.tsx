b9051dfd7c0e7870441636185b7576af42f474d4
Enmanuel <end199615@gmail.com>
2025-02-17 09:27:41 -0500

    Integración de boton y texto en app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import ImagePreview from "./components/Screens/imagePreview/ImagePreview";
 import Iconos from "./components/Screens/Formulario/Iconos";
 import BotonesEnmarcado from "./components/Screens/Formulario/BotonesEnmarcado";
+import SubirImagen from "./components/Screens/Formulario/SubirImagen";
 import styles from './App.module.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <div className={`${styles.column} ${styles.iconosColumn}`}>
         <Iconos />
         <BotonesEnmarcado />
+        <SubirImagen />
       </div>
     </div>
   </>
