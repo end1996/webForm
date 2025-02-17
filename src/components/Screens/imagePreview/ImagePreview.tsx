4cab5bf93ea0d94401df588e527ac9adaf4ade83
LuisMorenoMalpartida <gus2bmp@hotmail.com>
2025-02-17 10:55:00 -0500

    le quite el boton del contenedor, le agregue padding

--- src/components/Screens/imagePreview/ImagePreview.tsx
+++ src/components/Screens/imagePreview/ImagePreview.tsx
@@ -1,45 +1,38 @@
 import React, { useState } from 'react';
 import styles from './ImagePreview.module.css';
+import defaultImage from '../../../assets/arte.jpg'; // Corrige la ruta de la imagen por defecto
 
-const ImagePreview: React.FC = () => {
-    const [imageSrc, setImageSrc] = useState<string | null>(null);
+const ImagePreview = () => {
+    const [image, setImage] = useState(defaultImage); // Usa la imagen importada como valor inicial
 
     const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
             const reader = new FileReader();
-            reader.onloadend = () => {
-                setImageSrc(reader.result as string);
+            reader.onload = (e) => {
+                setImage(e.target?.result as string);
             };
             reader.readAsDataURL(file);
         }
     };
 
     return (
-        <div className={styles.card}>
-            <div className={styles.cardBody}>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                    className={styles.fileInput}
-                    id="fileInput"
-                />
-                {imageSrc && (
-                    <img
-                        className={styles.cardImg}
-                        src={imageSrc}
-                        alt="Imagen seleccionada"
-                    />
-                )}
+        <div>
+            <div className={styles.card}>
+                <div className={styles.cardBody}>
+                    <div className={styles.cardImg}>
+                        <img src={image} alt="Preview" />
+                    </div>
+                </div>
             </div>
-            {!imageSrc && (
-                <label htmlFor="fileInput" className={styles.uploadButton}>
-                    Subir Imagen
-                </label>
-            )}
+            <input
+                type="file"
+                className={styles.fileInput}
+                id="fileInput"
+                onChange={handleImageChange}
+            />
         </div>
     );
-}
+};
 
 export default ImagePreview;
