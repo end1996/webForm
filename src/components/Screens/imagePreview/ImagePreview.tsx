7f177e6a843d9da4cf411aa6f4ac578a30f7c8be
LuisMorenoMalpartida <gus2bmp@hotmail.com>
2025-02-17 10:08:26 -0500

    cambio pequeño

--- src/components/Screens/imagePreview/ImagePreview.tsx
+++ src/components/Screens/imagePreview/ImagePreview.tsx
@@ -1,16 +1,43 @@
+import React, { useState } from 'react';
 import styles from './ImagePreview.module.css';
-import arte from '../../../assets/arte.jpg';  // Importa la imagen
 
-function ImagePreview() {
+const ImagePreview: React.FC = () => {
+    const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImageSrc(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
         <div className={styles.card}>
             <div className={styles.cardBody}>
-                <img
-                    className={styles.cardImg}
-                    src={arte}  // Usa la imagen importada
-                    alt="Arte"
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className={styles.fileInput}
+                    id="fileInput"
                 />
+                {imageSrc && (
+                    <img
+                        className={styles.cardImg}
+                        src={imageSrc}
+                        alt="Imagen seleccionada"
+                    />
+                )}
             </div>
+            {!imageSrc && (
+                <label htmlFor="fileInput" className={styles.uploadButton}>
+                    Subir Imagen
+                </label>
+            )}
         </div>
     );
 }
