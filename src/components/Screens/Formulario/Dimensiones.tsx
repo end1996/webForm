2bbcdfbbb6012b640ea6ba35c1083c93b1575440
Enmanuel <end199615@gmail.com>
2025-02-17 11:09:49 -0500

    dimensiones, orientacion

--- src/components/Screens/Formulario/Dimensiones.tsx
+++ src/components/Screens/Formulario/Dimensiones.tsx
@@ -1,8 +1,21 @@
+import styles from './Dimensiones.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsLeftRight, faArrowsUpDown } from '@fortawesome/free-solid-svg-icons';
+
 function Dimensiones() {
     return (
-        <div>
-        <h1>Dimensiones</h1>
-        </div>
+        <>
+            <div>
+                <p className={styles.textTitle}>Dimensiones</p>
+                <p className={styles.textTitle}>Orientación</p>
+            </div>
+            <div className={styles.dimensionesContainer}>
+                <button className={styles.orientationButton}>
+                    <FontAwesomeIcon icon={faArrowsLeftRight} />Horizontal</button>
+                <button className={styles.orientationButton}>
+                    <FontAwesomeIcon icon={faArrowsUpDown} />Vertical</button>
+            </div>
+        </>
     );
 }
 
