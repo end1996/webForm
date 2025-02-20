61dd71920f769addebe83563372cf6a3c163dbd6
Enmanuel <end199615@gmail.com>
2025-02-20 12:07:58 -0500

    gestor de estado para mantener color en icono seleccionado

--- src/components/Screens/Formulario/Iconos/Iconos.tsx
+++ src/components/Screens/Formulario/Iconos/Iconos.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Iconos.module.css';
 
 interface IconosProps {
@@ -5,18 +6,25 @@ interface IconosProps {
 }
 
 const Iconos: React.FC<IconosProps> = ({ onIconClick }) => {
+  const [selectedIcon, setSelectedIcon] = useState<string>('main');
+
+  const handleIconClick = (view: string) => {
+    setSelectedIcon(view);
+    onIconClick(view);
+  };
+
   return (
     <div className={styles.iconContainer}>
       <div
-        className={styles.iconContainer__each}
-        onClick={() => onIconClick('main')}
+        className={`${styles.iconContainer__each} ${selectedIcon === 'main' ? styles.selected : ''}`}
+        onClick={() => handleIconClick('main')}
       >
         <i className="bi bi-card-image fs-4"></i>
         <label className={styles.iconText}>IMAGEN</label>
       </div>
       <div
-        className={styles.iconContainer__each}
-        onClick={() => onIconClick('marcos')}
+        className={`${styles.iconContainer__each} ${selectedIcon === 'marcos' ? styles.selected : ''}`}
+        onClick={() => handleIconClick('marcos')}
       >
         <i className="bi bi-file-person fs-4"></i>
         <label className={styles.iconText}>MARCO</label>
