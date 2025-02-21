486def9aeab0f35c763c195ca70534a316bc99e5
Enmanuel <end199615@gmail.com>
2025-02-21 09:43:36 -0500

    evento mousedown para cards de marcos

--- src/components/Screens/Formulario/Iconos/Marco/MarcosView.tsx
+++ src/components/Screens/Formulario/Iconos/Marco/MarcosView.tsx
@@ -1,33 +1,52 @@
 import { Card } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styles from './MarcosView.module.css';
 
 const MarcosView = () => {
+    // Crear un array con 12 elementos de texto 'Card 1', 'Card 2', 'Card 3', etc.
     const cards = Array.from({ length: 12 }, (_, index) => `Card ${index + 1}`);
     const marcoSize = ['1.5 cm', '2 cm', '3 cm', '4 cm'];
 
     const [selectedMarco, setSelectedMarco] = useState<string>('');
-    const [selectedSize, setSelectedSize] = useState<string>('')
+    const [selectedSize, setSelectedSize] = useState<string>('');
+    const ref = useRef<HTMLDivElement>(null);
 
+    // Manejar el cambio de marco seleccionado
     const handleMarcoChange = (marcoName: string) => {
         setSelectedMarco(marcoName)
     }
 
-    const handleMarcoSize = (sizeValue : string) => {
+    // Manejar el cambio de tamaño de marco seleccionado
+    const handleMarcoSize = (sizeValue: string) => {
         setSelectedSize(sizeValue)
     }
 
+    // Detectar clics fuera del componente para deseleccionar el marco
+    useEffect(() => {
+        function handleClickOutside(e: Event) {
+            if (ref.current && !ref.current.contains(e.target as Node)) {
+                setSelectedMarco('');
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [ref]);
+
     return (
-        <>
+        <div ref={ref}>
             <h2 className={styles.textTitle}>Marco</h2>
-            {/*<span className={styles.textSecondary}>Selecciona un marco para tu imagen</span>*/}
+            {/* Botones para seleccionar el tamaño del marco */}
             <div className={styles.buttonMarcoSizeContainer}>
-                { marcoSize.map( (size) => (
+                {marcoSize.map((size) => (
                     <button className={`${styles.buttonMarcoSize} ${selectedSize === size ? styles.selected : ''}`}
-                    onClick={() => handleMarcoSize(size)}
+                        onClick={() => handleMarcoSize(size)}
                     >{size}</button>
                 ))}
             </div>
+            {/* Mostrar las tarjetas con los marcos */}
             <div className={styles.marcosViewContainer}>
                 {cards.map((card, index) => {
                     return (
@@ -35,13 +54,14 @@ const MarcosView = () => {
                             className={`${styles.card} ${selectedMarco === card ? styles.selected : ''}`}
                             onClick={() => handleMarcoChange(card)}
                         >
+                            {/* Ruta de la imagen del marco */}
                             <Card.Img className={styles.cardImg} variant="top" src={`src/assets/marcos/marco-${index + 1}.jpg`} height={150} />
                             <Card.Body className={styles.cardBody}>{card}</Card.Body>
                         </Card>
                     );
                 })}
             </div>
-        </>
+        </div>
     );
 };
 
